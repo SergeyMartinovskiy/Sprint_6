a4b68026ae27cc8e4c7af2ef7709534cf50eb6cc
Ser Martin <ser.martinovskiy@yandex.ru>
2025-03-19 15:36:11 +0300

    First

---
+++ Sprint_6/Pages/header_page.py
@@ -0,0 +1,12 @@
+import allure
+from Locators.header_page_locators import HeaderPageLocator
+from
+
+
+class HeaderPage():
+    @allure.step('')
+    def click_logo_yandex(self):
+
+    @allure.step('')
+    def click_logo_samokat(self):
+
