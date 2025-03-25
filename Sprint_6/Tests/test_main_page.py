3655c6b12fb61839bb02838bb4aa647e8704222e
Ser Martin <ser.martinovskiy@yandex.ru>
2025-03-25 09:34:44 +0300

    First

--- Sprint_6/Tests/test_main_page.py
+++ Sprint_6/Tests/test_main_page.py
@@ -1,10 +1,28 @@
 import allure
 import pytest
 import data
-import URLS
 
-from Pages.base_page import BasePage
 from Pages.main_page import MainPage
-from conftest import general_settings
+
+
+class TestMainPageFAQ:
+    @allure.title('Проверка аккордеона FAQ - Вопрос/Ответ')
+    @pytest.mark.parametrize('question', data.ANSWERS)
+    def test_click_faq_and_get_answer(self, driver, question):
+        position, question = question
+        home_page = MainPage(driver)
+        home_page.click_button_accept_cookie()
+        home_page.scrolling_to_block_of_elements()
+        home_page.wait_show_up_element()
+        home_page.click_and_get_answer(position)
+        assert home_page.click_and_get_answer(position).text == question
+
+
+
+
+
+
+
+
 
 
